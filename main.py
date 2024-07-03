d8acd5a92f378964beb9c37e701947ff03f81387
Wilbert Misingo <wilbertmisingo@gmail.com>
2024-07-03 14:34:46 +0300

    added contacts

---
+++ main.py
@@ -0,0 +1,6 @@
+from tanzanian_banks import tanzanian_banks
+
+
+
+number_of_banks = tanzanian_banks.count()
+print(number_of_banks)
--- tanzanian_banks/__init__.py
+++ tanzanian_banks/__init__.py
@@ -2,14 +2,14 @@ import os
 import json
 
 
-
 class Banks(object):
     SYSTEM_PATH = os.getcwd()
-    BANKS_FILE = os
-    BRANCHES_FILE = os
+    BANKS_FILE = os.path.join(SYSTEM_PATH, "data/banks.json")
 
-    def get_file_data(self, file_path: str):
-        pass
+    def get_file_data(self, json_file_path: str):
+        with open(json_file_path, "r") as file:
+            data = json.load(file)
+        return data
 
     def get_banks(self):
         pass
@@ -23,13 +23,11 @@ class Banks(object):
     def get_mortigage_banks(self):
         pass
 
-
     def get_community_banks(self):
         pass
 
     def get_developmement_banks(self):
         pass
-
 
     def get_microfinace_banks(self):
         pass
@@ -40,5 +38,10 @@ class Banks(object):
     def get_regional_branches(self, bank_code: str, region_code: str):
         pass
 
+    def count(self):
+        data = self.get_file_data(self.BANKS_FILE)
+        amount = len(data["banks"])
+        return amount
 
 
+tanzanian_banks = Banks()
