f1aea61280cde8202ce4c7be4a6249abefd93d75
Wilbert Misingo <wilbertmisingo@gmail.com>
2024-07-03 12:16:54 +0300

    added banks

---
+++ tanzanian_banks/__init__.py
@@ -0,0 +1,44 @@
+import os
+import json
+
+
+
+class Banks(object):
+    SYSTEM_PATH = os.getcwd()
+    BANKS_FILE = os
+    BRANCHES_FILE = os
+
+    def get_file_data(self, file_path: str):
+        pass
+
+    def get_banks(self):
+        pass
+
+    def get_bank(self, bank_code: str):
+        pass
+
+    def get_commercial_banks(self):
+        pass
+
+    def get_mortigage_banks(self):
+        pass
+
+
+    def get_community_banks(self):
+        pass
+
+    def get_developmement_banks(self):
+        pass
+
+
+    def get_microfinace_banks(self):
+        pass
+
+    def get_branches(self, bank_code: str):
+        pass
+
+    def get_regional_branches(self, bank_code: str, region_code: str):
+        pass
+
+
+
