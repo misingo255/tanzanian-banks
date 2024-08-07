2829db4f939f587e53eac3104081e43cb3f66c99
Wilbert Misingo <wilbertmisingo@gmail.com>
2024-08-07 18:18:38 +0300

    added new functions

--- main.py
+++
@@ -1,6 +0,0 @@
-from tanzanian_banks import tanzanian_banks
-
-
-
-number_of_banks = tanzanian_banks.count()
-print(number_of_banks)
--- tanzanian_banks/__init__.py
+++ tanzanian_banks/__init__.py
@@ -25,51 +25,84 @@ class Banks(object):
                 return bank
         return {"message": "A bank with that code doesnt exist"}
 
+    def get_regional_town_council_banks(self):
+        banks = self.get_banks()
+        regional_town_council_banks = [
+            bank for bank in banks if bank["category"] == "regional town council bank"
+        ]
+        return regional_town_council_banks
+
+    def get_regional_town_municipal_banks(self):
+        banks = self.get_banks()
+        regional_town_municipal_banks = [
+            bank for bank in banks if bank["category"] == "regional town municipal bank"
+        ]
+        return regional_town_municipal_banks
+
+    def get_regional_town_municipal_outside_reginal_capital_banks(self):
+        banks = self.get_banks()
+        regional_town_municipal_outside_reginal_capital_banks = [
+            bank
+            for bank in banks
+            if bank["category"]
+            == "regional town municipal outside reginal capital bank"
+        ]
+        return regional_town_municipal_outside_reginal_capital_banks
+
+    def get_house_financing_banks(self):
+        banks = self.get_banks()
+        house_financing_banks = [
+            bank for bank in banks if bank["category"] == "house financing bank"
+        ]
+        return house_financing_banks
+
     def get_commercial_banks(self):
         banks = self.get_banks()
         commercial_banks = [
-            bank for bank in banks if bank["category"] == "Commercial Bank"
+            bank for bank in banks if bank["category"] == "commercial bank"
         ]
         return commercial_banks
 
-    def get_mortgage_banks(self):
+    def get_mortgage_financing_banks(self):
         banks = self.get_banks()
-        mortgage_banks = [bank for bank in banks if bank["category"] == "Mortgage Bank"]
-        return mortgage_banks
-
-    def get_community_banks(self):
-        banks = self.get_banks()
-        community_banks = [
-            bank for bank in banks if bank["category"] == "Community Bank"
+        mortgage_financing_bank = [
+            bank for bank in banks if bank["category"] == "mortigage refinancing bank"
         ]
-        return community_banks
+        return mortgage_financing_bank
+
+    def get_leasing_banks(self):
+        banks = self.get_banks()
+        leasing_banks = [bank for bank in banks if bank["category"] == "leasing bank"]
+        return leasing_banks
 
     def get_development_banks(self):
         banks = self.get_banks()
         development_banks = [
-            bank
-            for bank in banks
-            if bank["category"] == "Development Financial Institution"
+            bank for bank in banks if bank["category"] == "developement bank"
         ]
         return development_banks
 
     def get_microfinance_banks(self):
         banks = self.get_banks()
         microfinance_banks = [
-            bank for bank in banks if bank["category"] == "Microfinance Bank"
+            bank for bank in banks if bank["category"] == "microfinance bank"
         ]
         return microfinance_banks
-    
+
     def get_bank_contacts(self, bank_code: str):
+        pass
+
+    def get_all_branches_for_all_banks(self):
+        pass
+
+    def get_all_branches_for_all_banks_by_region(self, region_code: str):
         pass
 
     def get_bank_branches(self, bank_code: str):
         pass
 
-    def get_bank_regional_branches(self, bank_code: str, region_code: str):
+    def get_bank_branches_by_region(self, bank_code: str, region_code: str):
         pass
-
-
 
 
 tanzanian_banks = Banks()
