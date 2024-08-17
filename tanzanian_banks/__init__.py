e0a000a3706ae04db69a23089dda5ce8be9c250e
Wilbert Misingo <wilbertmisingo@gmail.com>
2024-08-17 21:30:38 +0300

    formatting

--- tanzanian_banks/__init__.py
+++ tanzanian_banks/__init__.py
@@ -3,12 +3,12 @@ import json
 
 
 class Banks(object):
-    SYSTEM_PATH = os.getcwd()
-    BANKS_FILE = os.path.join(SYSTEM_PATH, "data/banks.json")
-    BANKS_BRANCHES_FILE = os.path.join(SYSTEM_PATH, "data/branches.json")
-    BANKS_CONTACTS_FILE = os.path.join(SYSTEM_PATH, "data/contacts.json")
-    BANKS_REGIONS_FILE = os.path.join(SYSTEM_PATH, "data/regions.json")
-    BANKS_CATEGORIES_FILE = os.path.join(SYSTEM_PATH, "data/categories.json")
+    system_path = os.getcwd()
+    bank_file = os.path.join(system_path, "data/banks.json")
+    banks_branches_file = os.path.join(system_path, "data/branches.json")
+    banks_contacts_file = os.path.join(system_path, "data/contacts.json")
+    banks_regions_file = os.path.join(system_path, "data/regions.json")
+    banks_categories_file = os.path.join(system_path, "data/categories.json")
 
     def get_file_data(self, json_file_path: str):
         with open(json_file_path, "r") as file:
@@ -16,22 +16,22 @@ class Banks(object):
         return data
 
     def get_banks(self):
-        data = self.get_file_data(self.BANKS_FILE)
+        data = self.get_file_data(self.bank_file)
         banks = data["banks"]
         return banks
 
     def get_contacts(self):
-        data = self.get_file_data(self.BANKS_CONTACTS_FILE)
+        data = self.get_file_data(self.banks_contacts_file)
         contacts = data["contacts"]
         return contacts
-    
+
     def get_regions(self):
-        data = self.get_file_data(self.BANKS_REGIONS_FILE)
+        data = self.get_file_data(self.banks_regions_file)
         regions = data["regions"]
         return regions
-    
+
     def get_categories(self):
-        data = self.get_file_data(self.BANKS_CATEGORIES_FILE)
+        data = self.get_file_data(self.banks_categories_file)
         categories = data["categories"]
         return categories
 
@@ -40,7 +40,9 @@ class Banks(object):
         for bank in banks:
             if bank["code"] == bank_code:
                 return bank
-        return {"message": "Wrong bank code, please check the list of all banks to know its code"}
+        return {
+            "message": "Wrong bank code, please check the list of all banks to know its code"
+        }
 
     def get_regional_town_council_banks(self):
         banks = self.get_banks()
@@ -111,41 +113,56 @@ class Banks(object):
         contacts = [contact for contact in contacts if contact["code"] == bank_code]
         if contacts:
             return contacts
-        return {"message": "Wrong bank code, please check the list of all banks to know its code"}
+        return {
+            "message": "Wrong bank code, please check the list of all banks to know its code"
+        }
 
     def get_all_branches_for_all_banks(self):
-        data = self.get_file_data(self.BANKS_BRANCHES_FILE)
+        data = self.get_file_data(self.banks_branches_file)
         branches = data["branches"]
         return branches
-    
+
     def get_bank_branches(self, bank_code: str):
-       branches = self.get_all_branches_for_all_banks()
-       branches = [branch for branch in branches if branches["code"] == bank_code]
-       if branches:
+        branches = self.get_all_branches_for_all_banks()
+        branches = [branch for branch in branches if branches["code"] == bank_code]
+        if branches:
             return branches
-       return {"message": "Wrong bank code, please check the list of all banks to know its code"}
+        return {
+            "message": "Wrong bank code, please check the list of all banks to know its code"
+        }
 
     def get_all_branches_for_all_banks_by_region(self, region_code: str):
         regions = self.get_regions()
         region = [region for region in regions if regions["code"] == region_code]
         if not region:
-            return {"message": "Wrong region code, please check the list of all regions to know its code"}
+            return {
+                "message": "Wrong region code, please check the list of all regions to know its code"
+            }
         branches = self.get_all_branches_for_all_banks()
         branches = [branch for branch in branches if branches["region"] == region]
         if not branches:
-            return {"message": "Wrong region code, please check the list of all regions to know its code"}
+            return {
+                "message": "Wrong region code, please check the list of all regions to know its code"
+            }
         return branches
-
 
     def get_bank_branches_by_region(self, bank_code: str, region_code: str):
         regions = self.get_regions()
         region = [region for region in regions if regions["code"] == region_code]
         if not region:
-            return {"message": "Wrong region code, please check the list of all regions to know its code"}
+            return {
+                "message": "Wrong region code, please check the list of all regions to know its code"
+            }
         branches = self.get_all_branches_for_all_banks()
-        branches = [branch for branch in branches if branches["region"] == region and branches["code"] == bank_code]
+        branches = [
+            branch
+            for branch in branches
+            if branches["region"] == region and branches["code"] == bank_code
+        ]
         if not branches:
-            return {"message": "Wrong region code or bank code, please check the list of all regions and banks to know their code"}
+            return {
+                "message": "Wrong region code or bank code, please check the list of all regions and banks to know their code"
+            }
         return branches
 
 
